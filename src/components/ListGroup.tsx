362028d98f78216b280b832bef4379b609a82b7c
Aaron Joseph C. Baquing <aaronbaquing1@gmail.com>
2023-07-17 19:44:00 +0800

    state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,38 @@
-function ListGroup() {
-  let items = ["New York", "Los Angeles", "Chicago", "Tokyo", "Osaka"];
-  items = [];
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+
+  onSelectItem: (items: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  // States are independent from each other
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // Event handler
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
 
       {items.length === 0 && <p>There are no items!</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
